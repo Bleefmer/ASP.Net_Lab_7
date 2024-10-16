6befc44e3d2396987c0296e1d6727683eb379d7d
Bleefmer <118676325+Bleefmer@users.noreply.github.com>
2024-10-16 10:30:57 +0300

    Add files via upload

---
+++ Controllers/FileController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Text;
+
+namespace FileDownloadApp.Controllers
+{
+    public class FileController : Controller
+    {
+        [HttpGet]
+        public IActionResult DownloadFile()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DownloadFile(string firstName, string lastName, string fileName)
+        {
+
+            string fileContent = $"Ім'я: {firstName}\nПрізвище: {lastName}";
+
+            var fileStream = new MemoryStream(Encoding.UTF8.GetBytes(fileContent));
+
+            return File(fileStream, "text/plain", $"{fileName}.txt");
+        }
+    }
+}
